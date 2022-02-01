1b66bfa176445c626fcc2a4f46824321105e5cb6
Logiase <logiase.syx@gmail.com>
2022-02-01 23:42:18 +0800

    fix empty pages error

--- lib/core/locations.dart
+++ lib/core/locations.dart
@@ -1,8 +1,11 @@
 import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
+import 'package:hithere/providers/settings/settings_provider.dart';
 import 'package:hithere/views/home_page.dart';
 import 'package:hithere/views/schedule_view.dart';
+import 'package:hithere/views/settings_page.dart';
 import 'package:hithere/views/today_view.dart';
+import 'package:provider/provider.dart';
 import 'package:styled_widget/styled_widget.dart';
 
 class RootLocation extends BeamLocation<BeamState> {
@@ -13,23 +16,33 @@ class RootLocation extends BeamLocation<BeamState> {
         BeamGuard(
           pathPatterns: ['/'],
           check: (context, location) => false,
-          beamToNamed: (_, __) {
-            return '/home/today';
+          beamToNamed: (origin, target) {
+            return '/home';
           },
         ),
       ];
 
+  // @override
+  // Widget builder(BuildContext context, Widget navigator) => MultiProvider(
+  //       providers: [
+  //         ChangeNotifierProvider(create: (_) => SettingsProvider()),
+  //       ],
+  //       child: navigator,
+  //     );
+
   @override
-  List<Pattern> get pathPatterns => ['/', '/home/*'];
+  List<Pattern> get pathPatterns => ['/', '/home/*', '/settings'];
 
   @override
   List<BeamPage> buildPages(BuildContext context, BeamState state) {
     var ret = <BeamPage>[];
 
-    print(state.pathPatternSegments);
-
     if (state.pathPatternSegments.contains('home')) {
       ret.add(const BeamPage(key: ValueKey('home'), child: HomePage()));
+    }
+    if (state.pathPatternSegments.contains('settings')) {
+      //  ret.addAll(RootLocation(state.routeInformation).buildPages(context, state));
+      ret.add(const BeamPage(key: ValueKey('settings'), child: SettingsPage()));
     }
 
     return ret;
@@ -40,25 +53,39 @@ class HomeLocation extends BeamLocation<BeamState> {
   HomeLocation(RouteInformation routeInformation) : super(routeInformation);
 
   @override
+  List<BeamGuard> get guards => [
+        BeamGuard(
+            pathPatterns: ['/home'],
+            check: (_, __) => false,
+            beamToNamed: (_, __) => '/home/today'),
+      ];
+
+  @override
   List<Pattern> get pathPatterns => ['/home/today', '/home/schedule'];
 
   @override
   List<BeamPage> buildPages(BuildContext context, BeamState state) {
     var ret = <BeamPage>[];
 
-    print(state.pathPatternSegments);
-
     if (state.pathPatternSegments.contains('today')) {
       ret.add(const BeamPage(
         key: ValueKey('home-today'),
-        title: 'Today',
+        title: 'Home-Today',
         child: TodayView(),
       ));
     } else if (state.pathPatternSegments.contains('schedule')) {
       ret.add(const BeamPage(
         key: ValueKey('home-schedule'),
-        title: 'Schedule',
+        title: 'Home-Schedule',
         child: ScheduleView(),
+      ));
+    }
+
+    if (ret.isEmpty) {
+      ret.add(const BeamPage(
+        key: ValueKey('home-home'),
+        title: 'Home-Home',
+        child: Text('error'),
       ));
     }
 
