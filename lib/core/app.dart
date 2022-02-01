30734d22735363cb306acb546ac0f387418be444
Logiase <logiase.syx@gmail.com>
2022-02-01 23:42:01 +0800

    fix unknown '/' references error

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,20 +1,22 @@
 import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
-
-import 'package:hithere/core/locations.dart';
+import 'package:hithere/providers/settings/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class HitHereApp extends StatelessWidget {
-  HitHereApp({Key? key}) : super(key: key) {
-    debugPrint('HitHereApp created');
-  }
+  HitHereApp({Key? key, required this.routerDelegate}) : super(key: key);
+
+  final RouterDelegate<Object> routerDelegate;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: true,
       routeInformationParser: BeamerParser(),
-      routerDelegate: BeamerDelegate(locationBuilder: (routeInformation, _) {
-        return RootLocation(routeInformation);
-      }),
+      routerDelegate: routerDelegate,
+      themeMode: Provider.of<ThemeProvider>(context).themeMode,
+      darkTheme: ThemeData.dark(),
+      theme: ThemeData.light(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,29 @@
+import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-
 import 'package:hithere/core/app.dart';
+import 'package:hithere/core/locations.dart';
+import 'package:hithere/providers/settings/settings_provider.dart';
+import 'package:hithere/providers/settings/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  FlutterNativeSplash.removeAfter((context) async {
-  });
+  FlutterNativeSplash.removeAfter((context) async {});
 
-  runApp(HitHereApp());
+  final routerDelegate = BeamerDelegate(
+    locationBuilder: (routeInformation, _) {
+      return RootLocation(routeInformation);
+    },
+    initialPath: '/home',
+  );
+
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => SettingsProvider()),
+      ChangeNotifierProvider(create: (_) => ThemeProvider()),
+    ],
+    child: HitHereApp(
+      routerDelegate: routerDelegate,
+    ),
+  ));
 }
