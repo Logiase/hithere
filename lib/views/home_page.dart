0769fc6e09ed5e55f9cad4af97058d83a3ccf02a
Logiase <logiase.syx@gmail.com>
2022-02-02 23:11:42 +0800

    add AppBar to HomePage

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -9,6 +9,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       drawer: Drawer(
         child: Column(
           children: [
