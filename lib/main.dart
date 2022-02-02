761527af93c57054f40dbd98c6803f8245c05272
Logiase <logiase.syx@gmail.com>
2022-02-02 23:12:00 +0800

    open database at application start

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:hithere/core/app.dart';
 import 'package:hithere/core/locations.dart';
+import 'package:hithere/data/course_repository.dart';
+import 'package:hithere/data/course_service.dart';
+import 'package:hithere/providers/course_provider.dart';
 import 'package:hithere/providers/settings/global_settings.dart';
 import 'package:hithere/providers/settings/theme_settings.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  FlutterNativeSplash.removeAfter((context) async {});
+  CourseRepository courseRepository = CourseRepository();
+
+  FlutterNativeSplash.removeAfter((context) async {
+    await courseRepository.open();
+  });
 
   final routerDelegate = BeamerDelegate(
     locationBuilder: (routeInformation, _) {
@@ -21,6 +28,10 @@ void main() {
     providers: [
       ChangeNotifierProvider(create: (_) => GlobalSettings()),
       ChangeNotifierProvider(create: (_) => ThemeSettings()),
+      ChangeNotifierProvider(
+          create: (_) => CourseProvider(
+              courseRepository: courseRepository,
+              courseService: CourseService())),
     ],
     child: HitHereApp(
       routerDelegate: routerDelegate,
