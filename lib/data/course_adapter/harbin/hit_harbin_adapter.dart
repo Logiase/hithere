4a058d94812dc17c00cdc833ff8d49f847d850a2
Logiase <logiase.syx@gmail.com>
2022-02-03 15:07:17 +0800

    add HIT Harbin adapter [WIP]

---
+++ lib/data/course_adapter/harbin/hit_harbin_adapter.dart
@@ -0,0 +1,3 @@
+import 'package:hithere/data/course_adapter/course_adapter.dart';
+
+class HitHarbinAdapter extends CourseAdapter {}
---
+++ lib/data/course_adapter/harbin/postgraduate_parse.dart
@@ -0,0 +1,3 @@
+import 'package:hithere/data/course_adapter/course_adapter.dart';
+
+class PostGraduateParser extends CourseParser {}
