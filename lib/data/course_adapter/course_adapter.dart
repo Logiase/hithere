aebb29a97ac62bccc3f3b1a27a7fe7e10046827f
Logiase <logiase.syx@gmail.com>
2022-02-03 19:04:38 +0800

    update service interface

--- lib/data/course_adapter/course_adapter.dart
+++ lib/data/course_adapter/course_adapter.dart
@@ -1,5 +1,9 @@
+import 'package:hithere/models/course.dart';
+
 /// Course Adapter for different school location
-abstract class CourseAdapter {}
+abstract class CourseAdapter {
+  Future<List<Course>> fetchCourses(String semester);
+}
 
 /// Course parser interface for remote data
 abstract class CourseParser {}
--- lib/data/course_adapter/harbin/hit_harbin_adapter.dart
+++ lib/data/course_adapter/harbin/hit_harbin_adapter.dart
@@ -1,3 +1,14 @@
 import 'package:hithere/data/course_adapter/course_adapter.dart';
+import 'package:hithere/models/course.dart';
 
-class HitHarbinAdapter extends CourseAdapter {}
+class HitHarbinAdapter extends CourseAdapter {
+  String account = '';
+  String password = '';
+
+  String token = '';
+
+  @override
+  Future<List<Course>> fetchCourses(String semester) async {
+    return [];
+  }
+}
