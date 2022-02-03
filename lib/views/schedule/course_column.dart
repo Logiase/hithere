514af2562b048ad947368f36a8df2a53da7c1eac
Logiase <logiase.syx@gmail.com>
2022-02-03 14:58:28 +0800

    create course column

---
+++ lib/views/schedule/course_column.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:hithere/views/schedule/course.dart';
+import 'package:hithere/views/schedule/course_item.dart';
+import 'package:hithere/views/schedule/course_utils.dart';
+
+class CourseColumn extends StatelessWidget {
+  final double height;
+  final int rowCount;
+  final List<ScheduleCourse> courses;
+
+  const CourseColumn({
+    Key? key,
+    required this.height,
+    required this.rowCount,
+    required this.courses,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double minHeight = height / rowCount;
+
+    var processedCourses = CourseUtils.getProcessedCourses(courses, rowCount);
+
+    var children = <Widget>[];
+
+    for (var e in processedCourses) {
+      switch (e.type) {
+        case ProcessedCourseType.course:
+          children.add(CourseItem(courses: e.courses, minHeight: minHeight));
+          break;
+        case ProcessedCourseType.empty:
+          children.add(EmptyCourseItem(
+              start: e.courses[0].start,
+              step: e.courses[0].step,
+              minHeight: minHeight));
+          break;
+      }
+    }
+
+    return SizedBox(
+      height: height,
+      child: Column(children: children),
+    );
+  }
+}
