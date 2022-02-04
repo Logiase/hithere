1ee37f80b573c0f1fd2a40d29c13970c9afbf894
Logiase <logiase.syx@gmail.com>
2022-02-04 12:49:15 +0800

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,6 @@
-import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:hithere/core/app.dart';
-import 'package:hithere/core/locations.dart';
-import 'package:hithere/data/course_repository.dart';
-import 'package:hithere/data/course_service.dart';
-import 'package:hithere/providers/course_provider.dart';
-import 'package:hithere/providers/settings/global_settings.dart';
-import 'package:hithere/providers/settings/theme_settings.dart';
-import 'package:provider/provider.dart';
 
 void main() {
-  CourseRepository courseRepository = CourseRepository();
-  ThemeSettings themeSettings = ThemeSettings();
-
-  FlutterNativeSplash.removeAfter((context) async {
-    await courseRepository.open();
-    await themeSettings.load();
-  });
-
-  final routerDelegate = BeamerDelegate(
-    locationBuilder: (routeInformation, _) {
-      return RootLocation(routeInformation);
-    },
-    initialPath: '/home',
-    notFoundRedirectNamed: '/home',
-  );
-
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (_) => GlobalSettings()),
-      ChangeNotifierProvider(create: (_) => themeSettings),
-      ChangeNotifierProvider(
-          create: (_) => CourseProvider(
-              courseRepository: courseRepository,
-              courseService: CourseService())),
-    ],
-    child: HitHereApp(
-      routerDelegate: routerDelegate,
-    ),
-  ));
+  runApp(const HitHereApp());
 }
